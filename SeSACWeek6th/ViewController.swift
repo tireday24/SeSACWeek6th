b699677193e0971a0fa88ac87251915d75a33f43
Daniel Kwon <skansmfqh7312@gmail.com>
2022-08-08 09:30:01 +0900

    Initial Commit

---
+++ SeSACWeek6th/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACWeek6th
+//
+//  Created by 권민서 on 2022/08/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
